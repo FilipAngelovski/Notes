68a02a26cd3e656335c2098020952e5cc7cb737e
Filip Angelovski <f.angelovski88@gmail.com>
2018-03-09 17:28:20 +0100

    Adds edit note activity
    Adds EditNote activity.
    Adds icons for new note

--- app/src/main/java/com/notes/filip/notes/ListNotes.java
+++ app/src/main/java/com/notes/filip/notes/ListNotes.java
@@ -1,5 +1,6 @@
 package com.notes.filip.notes;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -27,8 +28,8 @@ public class ListNotes extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent intent = new Intent(ListNotes.this, EditNote.class);
+                startActivity(intent);
             }
         });
 
@@ -59,13 +60,7 @@ public class ListNotes extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
+        if (id == R.id.nav_manage) {
 
         } else if (id == R.id.nav_share) {
 
